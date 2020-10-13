af69645eb4e21887024e3e3d77673c4b891efea8
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-13 23:40:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 from FileOpening import *
 
 def main():
+    print("Please note that this code doesn't handle unicode characters. It only handles ASCII code and binary code.")
+    print("Using unicode characters might result in an error or a stack buffer overflow")
     print("Do you want to use an ascii file or a binary file? Conversion will be handled. \n")
     bin_or_not = input("Enter \"binary\" for a binary file and \"ascii\" for an ascii file: ")
     if bin_or_not == 'binary':
