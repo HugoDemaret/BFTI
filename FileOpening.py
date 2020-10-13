c688fcc06e185a57a51e4545174c389200dbc9b2
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-13 23:08:38 +0200

    Update FileOpening.py

--- FileOpening.py
+++ FileOpening.py
@@ -1,9 +1,20 @@
+from binascii import *
+def imagecreator(array):
+    pre_imagelength = len(array)
+    i = pre_imagelength // 2
+    print(array)
+    print("preimagelength = ", pre_imagelength)
+    print("i = ", i)
+
+
 def fileopening():
-    file_path = input("Enter you binary file path")
+    file_path = input("Enter your binary file path: ")
     file = open(file_path, "r+")
     lines = file.readlines()
     for line in lines:
         line_string = line
+        pre_image = []
         for letter in line_string:
-            print(letter)
-    file.close()
+            ascii_value = a2b_uu(letter)
+            pre_image.append(ascii_value)
+    imagecreator(pre_image)
