66241c4690e596da13f458aff772452804686966
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-13 23:32:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,14 @@
 from FileOpening import *
 
 def main():
-    fileopening()
+    print("Do you want to use an ascii file or a binary file? Conversion will be handled. \n")
+    bin_or_not = input("Enter \"binary\" for a binary file and \"ascii\" for an ascii file: ")
+    if bin_or_not == 'binary':
+        truth = 1
+    elif bin_or_not == 'ascii':
+        truth = 0
+    else:
+        print("You have entered the wrong value! Please retry!")
+        main()
+    fileopening(truth)
 main()
