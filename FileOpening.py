1551d941db8920ca7f691aaa6295612f92028841
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-13 23:51:44 +0200

    Update FileOpening.py

--- FileOpening.py
+++ FileOpening.py
@@ -13,18 +13,18 @@ def fileopening(int):
         file_path = input("Enter your binary file path: ")
         file = open(file_path, "r+")
         lines = file.readlines()
+        pre_image = []
         for line in lines:
             line_string = line
-            pre_image = []
             for letter in line_string:
                 pre_image.append(letter)
     else:
         file_path = input("Enter your ascii file path: ")
         file = open(file_path, "r+")
         lines = file.readlines()
+        pre_image = []
         for line in lines:
             line_string = line
-            pre_image = []
             for letter in line_string:
                 ascii_value = a2b_uu(letter) #Convert the caracters from ascii to binary code
                 pre_image.append(ascii_value)
