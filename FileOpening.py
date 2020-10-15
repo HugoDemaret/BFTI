0549ab288a548e5a62bcf0d28ba50dc0e8243554
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-15 22:14:32 +0200

    Update FileOpening.py

--- FileOpening.py
+++ FileOpening.py
@@ -3,11 +3,11 @@ import math
 
 def imagesize(array):
     pre_imagelength = len(array)
-    matrix_size = math.floor(math.sqrt(pre_imagelength+1))
+    matrix_size = math.floor(math.sqrt(pre_imagelength+1)) #Makes sure all the characters will fit inside the matrix
     return matrix_size
 def imagecreate(array, int):
     matrix_size = int
-    image = [[]*matrix_size]*matrix_size
+    image = [[]*matrix_size]*matrix_size # n x n matrix, need to fix the first 3 arrays which are empty
     elements = array
     for element in elements:
         for i in range(matrix_size):
@@ -32,6 +32,6 @@ def fileopening(int):
         for line in lines:
             line_string = line
             for letter in line_string:
-                ascii_value = a2b_uu(letter) #Convert the caracters from ascii to binary code
+                ascii_value = a2b_uu(letter) #Converts the caracters from ascii to binary code
                 pre_image.append(ascii_value)
     imagecreate(pre_image, imagesize(pre_image))
